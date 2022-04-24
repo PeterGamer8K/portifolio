10150582e39d52442bb51130c0691477d8480b7d
Pedro Meira <petergamer8k@gmail.com>
2022-04-24 19:43:55 -0300

    adicionando os botões de redes sociais

---
+++ pages/home/js/elementsController.js
@@ -0,0 +1,9 @@
+// My social networks buttons controller
+
+document.getElementById('githubButton').addEventListener('click', function() {
+    window.open("https://github.com/PeterGamer8K");
+});
+
+document.getElementById('linkedInButton').addEventListener('click', function() {
+    window.open("https://www.linkedin.com/in/pedro-meira220/");
+});
